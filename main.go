f075bba2f3bce0c31136a2c86a21fe047a0dfc5d
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-25 23:38:43 +0530

    code

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	//"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions"
 	//"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/robert/notification/handler"
 	"github.com/robert/notification/model"
 	"github.com/robert/notification/router"
 )
@@ -12,6 +13,8 @@ func main() {
 	model.OpenDatabaseConnection()
 	model.AutoMigrateModels()
 	r := gin.Default()
+	store := handler.Initsession()
+	r.Use(sessions.Sessions("mysession", store))
 	r.Static("/assets", "./assets")
 	r.LoadHTMLGlob("templates/*.html")
 	router.Startserver(r)
