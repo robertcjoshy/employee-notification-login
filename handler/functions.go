b4cf130eff0ec454b2b606ba655bc122db4a2291
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-25 23:38:20 +0530

    code

--- handler/functions.go
+++ handler/functions.go
@@ -1,8 +1,31 @@
 package handler
 
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
 func Authenticated(user, pass string) bool {
 	if user == "robert" && pass == "1234" {
 		return true
 	}
 	return false
+}
+
+func Initsession() sessions.Store {
+	store := cookie.NewStore([]byte("secret_key"))
+	return store
+}
+
+func Setsessions(c *gin.Context, key, value string) {
+	session := sessions.Default(c)
+	session.Set(key, value)
+	session.Save()
+}
+
+func clearsession(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
 }
