f408fc3c1de0ab4db281a025a9e38efd032bb35a
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-25 23:38:51 +0530

    code

--- middleware/middlewares.go
+++ middleware/middlewares.go
@@ -1,18 +1,30 @@
 package middleware
 
 import (
-	"net/http"
-
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
+/*
+	func Isloggedin() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			if _, err := c.Cookie("loggedin"); err == nil {
+				c.Next()
+			} else {
+				c.HTML(http.StatusOK, "login.html", gin.H{})
+				c.Abort()
+			}
+		}
+	}
+*/
 func Isloggedin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if _, err := c.Cookie("loggedin"); err == nil {
+		session := sessions.Default(c)
+		user := session.Get("user")
+
+		if user == nil {
 			c.Next()
-		} else {
-			c.HTML(http.StatusOK, "login.html", gin.H{})
-			c.Abort()
 		}
+
 	}
 }
